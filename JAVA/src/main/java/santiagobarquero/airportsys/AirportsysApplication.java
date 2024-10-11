dd9f907d60473a95c9e8fd7539d979be782499ff
santi <santiagobarquero1998@gmailcom>
2024-10-11 15:05:05 +0200

    [dev] spring initializer for the project

---
+++ JAVA/src/main/java/santiagobarquero/airportsys/AirportsysApplication.java
@@ -0,0 +1,13 @@
+package santiagobarquero.airportsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsysApplication.class, args);
+	}
+
+}
---
+++ JAVA/src/test/java/santiagobarquero/airportsys/AirportsysApplicationTests.java
@@ -0,0 +1,13 @@
+package santiagobarquero.airportsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
